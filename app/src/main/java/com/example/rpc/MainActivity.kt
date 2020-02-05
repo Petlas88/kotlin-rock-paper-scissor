cc8d949fd0207b6d829f00067862b8a7f50e8347
Petlas88 <lasse.pettersen88@gmail.com>
2020-02-05 21:07:22 +0100

    Added functionality to display choices etc

--- app/src/main/java/com/example/rpc/MainActivity.kt
+++ app/src/main/java/com/example/rpc/MainActivity.kt
@@ -8,8 +8,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private var scoreToWin:Int = 3
-    enum class Hand{ROCK, PAPER, SCISSORS}
+    private var scoreToWin: Int = 3
+
+    enum class Hand { ROCK, PAPER, SCISSORS, BLANK }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,17 +63,61 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     private fun playHands(p1: Player, p2: Player) {
-        if(p1.ready && p2.ready) {
+        if (p1.ready) {
+            p1_paper.visibility = View.INVISIBLE
+            p1_rock.visibility = View.INVISIBLE
+            p1_scissor.visibility = View.INVISIBLE
+            p1_random.visibility = View.INVISIBLE
+        }
+
+        if (p2.ready) {
+            p2_paper.visibility = View.INVISIBLE
+            p2_rock.visibility = View.INVISIBLE
+            p2_scissor.visibility = View.INVISIBLE
+            p2_random.visibility = View.INVISIBLE
+        }
+
+
+
+
+
+        if (p1.ready && p2.ready) {
             calculateRoundResult(p1, p2)
             println("${p1.wins}:${p2.wins}")
+            when (p1.hand) {
+                Hand.ROCK -> p1_hand.setImageResource(R.drawable.rock)
+                Hand.PAPER -> p1_hand.setImageResource(R.drawable.paper)
+                Hand.SCISSORS -> p1_hand.setImageResource(R.drawable.scissor)
 
-            if(p1.wins == scoreToWin) {
+            }
+
+            when (p2.hand) {
+                Hand.ROCK -> p2_hand.setImageResource(R.drawable.rock)
+                Hand.PAPER -> p2_hand.setImageResource(R.drawable.paper)
+                Hand.SCISSORS -> p2_hand.setImageResource(R.drawable.scissor)
+            }
+
+            next_btn.visibility = View.VISIBLE
+            next_btn.setOnClickListener() {
+                p1_paper.visibility = View.VISIBLE
+                p1_rock.visibility = View.VISIBLE
+                p1_scissor.visibility = View.VISIBLE
+                p2_paper.visibility = View.VISIBLE
+                p2_rock.visibility = View.VISIBLE
+                p2_scissor.visibility = View.VISIBLE
+                p1_random.visibility = View.VISIBLE
+                p2_random.visibility = View.VISIBLE
+                p2_hand.setImageDrawable(null)
+                p1_hand.setImageDrawable(null)
+                next_btn.visibility = View.INVISIBLE
+            }
+
+            if (p1.wins == scoreToWin) {
                 p1.winner = true
                 showWinner(p1, p2)
 
-            } else if(p2.wins == scoreToWin) {
+            } else if (p2.wins == scoreToWin) {
                 p2.winner = true
                 showWinner(p1, p2)
 
@@ -85,20 +130,22 @@ class MainActivity : AppCompatActivity() {
             println("Both no ready")
         }
     }
-    private fun calculateRoundResult(p1:Player, p2:Player) {
+
+    private fun calculateRoundResult(p1: Player, p2: Player) {
 
         if (p1.hand == p2.hand) {
             println("Tie!")
         } else {
-            when(p1.hand) {
+            when (p1.hand) {
                 Hand.PAPER -> if (p2.hand == Hand.ROCK) p1.wins++ else p2.wins++
                 Hand.ROCK -> if (p2.hand == Hand.SCISSORS) p1.wins++ else p2.wins++
                 Hand.SCISSORS -> if (p2.hand == Hand.PAPER) p1.wins++ else p2.wins++
             }
         }
     }
-    private fun getRandomHand():Hand {
-        return when(Random.nextInt(0, 2)) {
+
+    private fun getRandomHand(): Hand {
+        return when (Random.nextInt(0, 2)) {
             0 -> Hand.SCISSORS
             1 -> Hand.ROCK
             2 -> Hand.PAPER
@@ -106,29 +153,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showWinner(p1:Player, p2:Player) {
+    private fun showWinner(p1: Player, p2: Player) {
         win_txt.visibility = View.VISIBLE
-        p1_score.visibility  = View.INVISIBLE
-        p2_score.visibility  = View.INVISIBLE
-        score_dash.visibility  = View.INVISIBLE
-        p1_paper.visibility = View.INVISIBLE
-        p1_rock.visibility = View.INVISIBLE
-        p1_scissor.visibility = View.INVISIBLE
-        p2_paper.visibility = View.INVISIBLE
-        p2_rock.visibility = View.INVISIBLE
-        p2_scissor.visibility = View.INVISIBLE
-        p1_random.visibility = View.INVISIBLE
-        p2_random.visibility = View.INVISIBLE
+        p1_score.visibility = View.INVISIBLE
+        p2_score.visibility = View.INVISIBLE
+        score_dash.visibility = View.INVISIBLE
         again_button.visibility = View.VISIBLE
+
         again_button.setOnClickListener {
             p1.wins = 0
             p2.wins = 0
             p1_score.text = p1.wins.toString()
             p2_score.text = p2.wins.toString()
             win_txt.visibility = View.INVISIBLE
-            p1_score.visibility  = View.VISIBLE
-            p2_score.visibility  = View.VISIBLE
-            score_dash.visibility  = View.VISIBLE
+            p1_score.visibility = View.VISIBLE
+            p2_score.visibility = View.VISIBLE
+            score_dash.visibility = View.VISIBLE
             p1_paper.visibility = View.VISIBLE
             p1_rock.visibility = View.VISIBLE
             p1_scissor.visibility = View.VISIBLE
@@ -141,7 +181,7 @@ class MainActivity : AppCompatActivity() {
         }
         if (p1.winner) {
             win_txt.text = "Blue wins!"
-            win_txt.setTextColor(Color.rgb(127,143,250))
+            win_txt.setTextColor(Color.rgb(127, 143, 250))
         }
         if (p2.winner) {
             win_txt.text = "Red wins!"
--- app/src/main/java/com/example/rpc/Player.kt
+++ app/src/main/java/com/example/rpc/Player.kt
@@ -1,7 +1,7 @@
 package com.example.rpc
 
 class Player {
-    var hand: MainActivity.Hand = MainActivity.Hand.PAPER
+    var hand: MainActivity.Hand = MainActivity.Hand.BLANK
     var wins:Int = 0
     var winner:Boolean = false
     var ready:Boolean = false
